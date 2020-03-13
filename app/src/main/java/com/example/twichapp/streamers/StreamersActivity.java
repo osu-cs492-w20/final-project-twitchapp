87d3f547c88bfec70e030af20570035dbfcbce11
gs291 <sanchez.gregory886@gmail.com>
2020-03-12 19:51:09 -0700

    Update activity to include recycler view for twitch streams

--- app/src/main/java/com/example/twichapp/streamers/StreamersActivity.java
+++ app/src/main/java/com/example/twichapp/streamers/StreamersActivity.java
@@ -1,21 +1,46 @@
-package com.example.twichapp;
+package com.example.twichapp.streamers;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.twichapp.FavoritesActivity;
+import com.example.twichapp.MainActivity;
+import com.example.twichapp.R;
+import com.example.twichapp.data.Status;
+import com.example.twichapp.data.TwitchStream;
 import com.google.android.material.navigation.NavigationView;
 
-public class StreamersActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.List;
+
+public class StreamersActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, StreamersAdapter.OnStreamerClickListener {
+    private static final String TAG = StreamersActivity.class.getSimpleName();
+
     private DrawerLayout mDrawerLayout;
+    private RecyclerView mStreamersRV;
+    private ProgressBar mLoadingIndicatorPB;
+    private TextView mLoadingErrorMessageTV;
+
+    private StreamersAdapter mStreamersAdapter;
+    private StreamersViewModel mStreamersViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +59,56 @@ public class StreamersActivity extends AppCompatActivity implements NavigationVi
         textView.setText(getString(R.string.page_streamers));
         actionBar.setDisplayShowTitleEnabled(false);
 
+        mLoadingIndicatorPB = findViewById(R.id.pb_loading_indicator);
+        mLoadingErrorMessageTV = findViewById(R.id.tv_loading_error_message);
+        mStreamersRV = findViewById(R.id.rv_twitch_stream_items);
+
+        mStreamersAdapter = new StreamersAdapter(this);
+        mStreamersRV.setAdapter(mStreamersAdapter);
+        mStreamersRV.setLayoutManager(new LinearLayoutManager(this));
+        mStreamersRV.setHasFixedSize(true);
+
         mDrawerLayout = findViewById(R.id.drawer_layout_streamers);
+
+        mStreamersViewModel = new ViewModelProvider(this).get(StreamersViewModel.class);
+
+        mStreamersViewModel.getStreams().observe(this, new Observer<List<TwitchStream>>() {
+            @Override
+            public void onChanged(List<TwitchStream> twitchStreams) {
+                mStreamersAdapter.updateTwitchStreams(twitchStreams);
+                if (twitchStreams != null) {
+                    mStreamersAdapter.updateTwitchStreams(twitchStreams);
+                }
+            }
+        });
+
+        mStreamersViewModel.getLoadingStatus().observe(this, new Observer<Status>() {
+            @Override
+            public void onChanged(@Nullable Status status) {
+                if (status == Status.LOADING) {
+                    mLoadingIndicatorPB.setVisibility(View.VISIBLE);
+                } else if (status == Status.SUCCESS) {
+                    mLoadingIndicatorPB.setVisibility(View.INVISIBLE);
+                    mLoadingErrorMessageTV.setVisibility(View.INVISIBLE);
+                    mStreamersRV.setVisibility(View.VISIBLE);
+                } else {
+                    mLoadingIndicatorPB.setVisibility(View.INVISIBLE);
+                    mStreamersRV.setVisibility(View.INVISIBLE);
+                    mLoadingErrorMessageTV.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         NavigationView navigationView = findViewById(R.id.nv_nav_drawer_streamers);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button button = findViewById(R.id.btn_test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStreamersViewModel.loadStreams("");
+            }
+        });
     }
 
     @Override
@@ -68,4 +139,8 @@ public class StreamersActivity extends AppCompatActivity implements NavigationVi
         }
     }
 
+    @Override
+    public void onStreamerClick(TwitchStream twitchStream) {
+
+    }
 }
