b4c97e1e3b50de91a85c0d17b8eae22db220b8a1
gs291 <sanchez.gregory886@gmail.com>
2020-03-12 19:48:20 -0700

    Add Twitch Client-ID to headers

--- app/src/main/java/com/example/twichapp/utils/NetworkUtils.java
+++ app/src/main/java/com/example/twichapp/utils/NetworkUtils.java
@@ -1,5 +1,7 @@
 package com.example.twichapp.utils;
 
+import com.example.twichapp.BuildConfig;
+
 import java.io.IOException;
 
 import okhttp3.OkHttpClient;
@@ -10,7 +12,10 @@ public class NetworkUtils {
     private static final OkHttpClient mHTTPClient = new OkHttpClient();
 
     public static String doHttpGet(String url) throws IOException {
-        Request request = new Request.Builder().url(url).build();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Client-ID", BuildConfig.TWITCH_API_CLIENT_ID)
+                .build();
         Response response = mHTTPClient.newCall(request).execute();
         try {
             return response.body().string();
